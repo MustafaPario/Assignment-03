efe2eeb3e993d9b102b7e843d2f9301bc3c3de01
MustafaPario <mustafapario22@gmail.com>
2023-07-05 16:35:59 +0500

    add question 05

---
+++ question-5.dart
@@ -0,0 +1,9 @@
+void main() {
+  int number = 10;
+  int result = 0;
+  for (int i = 1; i <= number; i++) {
+    result = result + i;
+  }
+
+  print('sum of all the digits $number is: $result');
+}
